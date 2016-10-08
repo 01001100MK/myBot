edb00dd7081f6f22d585cdeb6fb9f31eddd2dd1f
Lin Myat Ko <lko@localhost.localdomain>
2016-10-09 00:11:21 +0630

    movieApi

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.post('/webhook', function(req, res) {
             } else if (text.substring(0, 2).toLowerCase() === 'mv') {
                 var movie = text.substring(3, text.length);
                 if (movie) {
-                    movieApi.searchMovie(sender, movie);
+                    searchMovie(sender, movie);
                 }
             } else {
                 sendTextMessage(sender, "This is Alfred! Don't mess up with me!");
@@ -305,3 +305,23 @@ function getDictionary(sender, word) {
             }
         });
 }
+
+searchMovie = function(sender, movie) {
+    request
+        .get("https://api.themoviedb.org/3/search/movie?api_key=81d7640dffed48055b1803be5b452893&query=" + movie)
+        .set('Content-Type', 'application/json')
+        .accept('application/json')
+        .end(function(err, res) {
+            if (err) {
+                console.error(err);
+            } else {
+                var movie = res.body.results[0];
+
+                sendTextMessage(sender, movie.title);
+                sendTextMessage(sender, movie.overview);
+                sendTextMessage(sender, movie.release_date);
+                sendTextMessage(sender, movie.vote_average);
+                sendTextMessage(sender, movie.id);
+            }
+        });
+};
